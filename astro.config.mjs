6c30eb9785030470e043af4a748879a942829338
Tony Mamo <tmamo13991@gmail.com>
2026-01-11 15:42:25 -0500

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,22 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config"
+
+import react from "@astrojs/react"
+import netlify from "@astrojs/netlify"
+import sitemap from "@astrojs/sitemap"
+import tailwindcss from "@tailwindcss/vite"
+import icon from "astro-icon"
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://marevo-labs.dev",
+  integrations: [react(), sitemap(), icon()],
+  adapter: netlify(),
+  vite: {
+    plugins: [
+      tailwindcss({
+        base: "./src/global.css",
+      }),
+    ],
+  },
+})
